d9fa87b5b892ab4f814dfaa06d8de1868fbfc941
Win Holzapfel <wholzapfel@hotmail.com>
2018-10-21 22:09:27 +1100

    night night winnie will sleep tight

--- public/js/test.js
+++ public/js/test.js
@@ -74,3 +74,63 @@ function stopSoundB() {
 		document.getElementById('testArea').innerHTML += data;
 	}); //The get request is sent to the server
 }
+
+function beginChain() {
+	//This function begins the test
+}
+
+function beginInversion() {
+	//Invert the audio wave that was uploaded
+	document.getElementById('testAreaInversion').innerHTML = "<p>If your audio output chain is working right, you should hear nothing in the second button below.</p><button class='btn' onclick='playSound()'>Play</button><button class='btn' onclick='playInvertedSound()'>Play Inverted</button>";
+}
+
+var soundPlay = 0;
+
+
+//Get the audio context. This audio processor is responsible for changing the audio to be inverted
+var context = Pizzicato.context;
+var scriptNode = context.createScriptProcessor(2048, 1, 1);
+
+//Sample code for processing a particular audio context
+scriptNode.onaudioprocess = function(audioProcessingEvent) {
+  // The input buffer is the song we loaded earlier
+  var inputBuffer = audioProcessingEvent.inputBuffer;
+
+  // The output buffer contains the samples that will be modified and played
+  var outputBuffer = audioProcessingEvent.outputBuffer;
+
+  // Loop through the output channels (in this case there is only one)
+  for (var channel = 0; channel < outputBuffer.numberOfChannels; channel++) {
+    var inputData = inputBuffer.getChannelData(channel);
+    var outputData = outputBuffer.getChannelData(channel);
+
+    // Loop through the samples
+    for (var sample = 0; sample < inputBuffer.length; sample++) {
+      // make output equal to the negation of the input
+      outputData[sample] = -inputData[sample];
+
+      // add noise to each output sample
+      outputData[sample] += ((Math.random() * 2) - 1) * 0.1;
+    }
+  }
+}
+
+function playSound() {
+		soundPlay = new Pizzicato.Sound('./audio/tremolo-guitar.mp3', function() {
+			soundPlay.play();
+		});
+}
+
+function playInvertedSound() {
+	//Basic sound inversion filter
+	soundPlay.stop();
+
+	soundPlay.getSourceNode().connect(scriptNode);
+	scriptNode.connect(context.destination);
+	soundPlay.play();
+
+	sound.on('end', function() {
+		soundPlay.getSourceNode().disconnect(scriptNode);
+		scriptNode.disconnect(context.destination);
+	});
+}
