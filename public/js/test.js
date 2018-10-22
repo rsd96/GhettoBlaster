6516db6ac04023d7147da511371474aa030c31d4
code <cody9m@gmail.com>
2018-10-22 17:39:54 +1100

    Update public/js/test.js
    Fixed error where displayed rate of change of frequency was off by a factor of 10

--- public/js/test.js
+++ public/js/test.js
@@ -14,7 +14,7 @@ var sineWave = new Pizzicato.Sound({
 
 function begin() {
 	document.getElementById('beginBtn').style.display = 'none';
-	document.getElementById('testArea').innerHTML = "<h3>Test 1</h3><p>Tests the high frequency cutoff of audio hardware. Press the button when you can't hear the tone. NOTE: Will be different between people, perform test yourself.</p><button class='btn' onclick='stopSoundA()'>Stop</button><p id='Help'>2400Hz/Sec</p><p id='Freq'></p>";
+	document.getElementById('testArea').innerHTML = "<h3>Test 1</h3><p>Tests the high frequency cutoff of audio hardware. Press the button when you can't hear the tone. NOTE: Will be different between people, perform test yourself.</p><button class='btn' onclick='stopSoundA()'>Stop</button><p id='Help'>240Hz/Sec</p><p id='Freq'></p>";
 	sineWave.play();
 	interval = setInterval(increaseFrequency, 100);
 }
@@ -33,7 +33,7 @@ function stopSoundA() {
 	if (counter < 4) {
 		accuracy = accuracy/2;
 		sineWave.frequency -= 200;
-		document.getElementById('Help').innerHTML = (accuracy*100) + "Hz/Sec";
+		document.getElementById('Help').innerHTML = (accuracy*10) + "Hz/Sec";
 		counter++;
 		return;
 	}
@@ -46,7 +46,7 @@ function stopSoundA() {
 }
 
 function startSoundB() {
-	document.getElementById('testArea').innerHTML = "<h3>Test 2</h3><p>Tests the low frequency cutoff of audio hardware. Press the button when you can't hear the tone.</p><button class='btn' onclick='stopSoundB()'>Stop</button><p id='Help'>2400Hz/Sec</p><p id='Freq'></p>";
+	document.getElementById('testArea').innerHTML = "<h3>Test 2</h3><p>Tests the low frequency cutoff of audio hardware. Press the button when you can't hear the tone.</p><button class='btn' onclick='stopSoundB()'>Stop</button><p id='Help'>240Hz/Sec</p><p id='Freq'></p>";
 	sineWave.frequency = 5000;
 	sineWave.play();
 	interval = setInterval(decreaseFrequency, 100);
@@ -56,7 +56,7 @@ function stopSoundB() {
 	if (counter < 4) {
 		accuracy = accuracy/2;
 		sineWave.frequency += 200;
-		document.getElementById('Help').innerHTML = (accuracy*100) + "Hz/Sec";
+		document.getElementById('Help').innerHTML = (accuracy*10) + "Hz/Sec";
 		counter++;
 		return;
 	}
