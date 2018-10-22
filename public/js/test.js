6ed3a79c1c0213f4dad312279a3d547619bf87f8
Win Holzapfel <wholzapfel@hotmail.com>
2018-10-22 23:03:27 +1100

    Added filter chaining test to the metamorphic tests field

--- public/js/test.js
+++ public/js/test.js
@@ -75,12 +75,101 @@ function stopSoundB() {
 	}); //The get request is sent to the server
 }
 
+var startFreq = 1000;
+var endFreq = 2000;
+
 function beginChain() {
-	//This function begins the test
+	//This function begins the test for audio chaining
+	document.getElementById('beginBtn3').style.display = 'none';
+	document.getElementById('testAreaChain').innerHTML = "<p>We test a low pass filter, then add a high pass filter to restrict the audio range. We can't tell if this is correct, so we reverse the order.</p>\
+	<p>The test runs from 100Hz-3000Hz. Additional test cases are generated every time the button below is pressed.</p>\
+	<button class='btn' onclick='changeFreq()'>Generate new frequency range</button><span id='freqRange'>" + startFreq + "Hz - " + endFreq + "Hz</span><br />\
+	<button class='btn' onclick='playLowpassFirst()'>Play with lowpass first</button>\
+	<button class='btn' onclick='playHighpassFirst()'>Play with highpass first</button>";
+
+}
+
+var generatorSound = new Pizzicato.Sound({
+	source: "wave",
+	options: {
+		type: "sawtooth",
+		frequency: 100
+	}
+});
+
+var lowPassFilter = new Pizzicato.Effects.LowPassFilter({
+		frequency: startFreq,
+		peak: 10
+});
+
+var highPassFilter = new Pizzicato.Effects.HighPassFilter({
+		frequency: endFreq,
+		peak: 10
+});
+
+var generatorFreq = 0;
+
+function increaseGeneratorFrequency() {
+	generatorSound.frequency += 50;
+
+	if (generatorSound.frequency > 3000) {
+		clearInterval(generatorFreq);
+		generatorSound.stop();
+	}
+}
+
+function playHighpassFirst() {
+	if (generatorFreq != 0) clearInterval(generatorFreq);
+	generatorSound.frequency = 100;
+
+	lowPassFilter.frequency = startFreq;
+	highPassFilter.frequency = endFreq;
+
+	generatorSound.removeEffect(highPassFilter);
+	generatorSound.removeEffect(lowPassFilter);
+
+	generatorSound.addEffect(highPassFilter);
+	generatorSound.addEffect(lowPassFilter);
+
+	generatorSound.play();
+
+	generatorFreq = setInterval(increaseGeneratorFrequency, 100);
+}
+
+function playLowpassFirst() {
+	//Use a Sin generator, generating the lowpass filter first then the highpass filter
+		/*var lowPassFilter = new Pizzicato.Effects.LowPassFilter({
+	    frequency: 400,
+	    peak: 10
+		});*/
+		if (generatorFreq != 0) clearInterval(generatorFreq);
+		generatorSound.frequency = 100;
+
+		lowPassFilter.frequency = startFreq;
+		highPassFilter.frequency = endFreq;
+
+		generatorSound.removeEffect(lowPassFilter);
+		generatorSound.removeEffect(highPassFilter);
+
+		generatorSound.addEffect(lowPassFilter);
+		generatorSound.addEffect(highPassFilter);
+
+		generatorSound.play();
+
+		generatorFreq = setInterval(increaseGeneratorFrequency, 100);
+}
+
+function changeFreq() {
+	startFreq = Math.round(Math.random()*3000);
+	endFreq = Math.round(Math.random()*3000);
+
+	//After random generation, we simply use this random frequency range
+	document.getElementById("freqRange").innerHTML = startFreq + "Hz - " + endFreq + "Hz";
 }
 
 function beginInversion() {
 	//Invert the audio wave that was uploaded
+	document.getElementById('beginBtn2').style.display = 'none';
 	document.getElementById('testAreaInversion').innerHTML = "<p>If your audio output chain is working right, you should hear nothing but static in the second button below.</p><button class='btn' onclick='playSound()'>Play</button><button class='btn' onclick='playInvertedSound()'>Play Inverted</button>";
 }
 
